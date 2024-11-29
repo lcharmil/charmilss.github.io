97fea67b6717c6efb5d4dde26f261ee784872fe8
lcharmil <charmillaheri@gmail.com>
2024-11-28 22:18:30 -0500

    Define Parent Shape Class and Ball Class

--- A4PART4.js
+++ A4PART4.js
@@ -1,3 +1,4 @@
+// Constants 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
@@ -6,4 +7,45 @@ const height = canvas.height = window.innerHeight;
 // Helper function to generate random numbers
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Parent Shape class
+class Shape {
+    constructor(x, y, velX, velY) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
+
+// Ball class (inherits from Shape)
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size) {
+        super(x, y, velX, velY); // Call to Shape constructor
+        this.color = color;
+        this.size = size;
+    }
+
+    // Draw the ball
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    // Update ball's position and detect boundaries
+    update() {
+        if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+            this.velX = -this.velX;
+        }
+
+        if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+            this.velY = -this.velY;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
 }
