c1952e232d396307bad9dbb9da6f8d3ff2dc26f0
lcharmil <charmillaheri@gmail.com>
2024-11-29 01:27:02 -0500

    Dynamic Thumbnail Creation

---
+++ A4PART2.js
@@ -0,0 +1,15 @@
+// Array of image file names
+const imageFiles = ['A4PART2_1.jpg', 'A4PART2_2.jpg', 'A4PART2_3.jpg', 'A4PART2_4.jpg', 'A4PART2_5.jpg'];
+
+// DOM Elements
+const thumbsContainer = document.querySelector('.thumbs');
+const displayedImage = document.getElementById('displayed-image');
+
+// Dynamically create thumbnails
+imageFiles.forEach((file, index) => {
+    const thumb = document.createElement('img');
+    thumb.src = `images/${file}`;
+    thumb.alt = `Thumbnail ${index + 1}`;
+    thumb.classList.add('thumbnail');
+    thumbsContainer.appendChild(thumb);
+});
